069396de53b0461cd2be7a94904c630208b330cc
De Xian <ahx.agent007@gmail.com>
2019-06-16 02:19:04 +0600

    Work Started

--- app/src/main/java/com/secretdevelopersltd/dexian/customhome/MainActivity.java
+++ app/src/main/java/com/secretdevelopersltd/dexian/customhome/MainActivity.java
@@ -2,12 +2,48 @@ package com.secretdevelopersltd.dexian.customhome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG= "XIAN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.main_menu, popup.getMenu());
+        popup.show();
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.setting:
+                        //your code
+                        // EX : call intent if you want to swich to other activity
+                        Log.i(TAG,"setting");
+                        return true;
+                    case R.id.about:
+                        Log.i(TAG,"about");
+                        //your code
+                        return true;
+                    default:
+                        return false;
+                }
+
+            }
+        });
     }
 }
